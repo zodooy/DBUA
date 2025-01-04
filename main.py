3b25b0f8696daebf29c6fc6efcce6a419264919f
Zhengyang_Li <zodooy@gmail.com>
2025-01-04 17:49:39 +0800

    add main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from scripts.dbua import dbua
 from utilities.data import SAMPLE, LOSS, CTRUE
 
 if __name__ == "__main__":
-    # dbua(SAMPLE, LOSS)
+    dbua(SAMPLE, LOSS)
 
-    # Run all examples
-    for sample in CTRUE.keys():
-        print(sample)
-        dbua(sample, LOSS)
+    # # Run all examples
+    # for sample in CTRUE.keys():
+    #     print(sample)
+    #     dbua(sample, LOSS)
