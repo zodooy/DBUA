3de9ef73870994efa4b6d078a3b7d0edbfb9e042
Zhengyang_Li <zodooy@gmail.com>
2025-01-05 16:20:06 +0800

    delete test code

--- scripts/dbua.py
+++ scripts/dbua.py
@@ -134,7 +134,6 @@ def dbua(sample, loss_name):
 
 
     # Create the optimizer
-    c = 1490.0 * to_cuda(torch.ones((SOUND_SPEED_NXC, SOUND_SPEED_NZC))) # test sound speed
     c = c.clone().detach().requires_grad_(True)
     optimizer = optim.Adam([c], lr=LEARNING_RATE, amsgrad=True)
 
